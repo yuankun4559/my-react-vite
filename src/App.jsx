21c7cf4015eac70d9c816ee7b8abd79d00a32c19
yuankun <yuankun0310@126.com>
2023-05-18 17:07:04 +0800

    init project

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+/*
+ * @Author: yuankun yuankun0310@126.com
+ * @Date: 2022-01-21 15:38:16
+ * @LastEditors: yuankun yuankun0310@126.com
+ * @LastEditTime: 2022-07-05 15:30:51
+ * @FilePath: /vite-first-project/src/App.jsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const envName = import.meta.env;
+  console.log(envName, "??????");
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
